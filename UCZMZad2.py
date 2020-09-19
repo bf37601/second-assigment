d285d685f41d4dee85afd1ae8a8ffd9ea3f76be0
bf37601 <bf37601@st.amu.edu.pl>
2020-09-19 15:27:03 +0200

    #Task 2

--- UCZMZad2.py
+++ UCZMZad2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
 df_public = pd.read_csv('survey_results_public.csv',
                         usecols=['Respondent',
@@ -7,10 +8,9 @@ df_public = pd.read_csv('survey_results_public.csv',
                                  'YearsCode',
                                  'YearsCodePro',
                                  'ConvertedComp',
-                                 'Age'
-                                 # 'BetterLife',
-                                 # 'JobSat'
-                                 ],
+                                 'Age',
+                                 'BetterLife',
+                                 'JobSat'],
                         index_col='Respondent')
 df_public.dropna(inplace=True)
 
@@ -26,15 +26,14 @@ df_public.replace(to_replace='3e+05', value='300000', inplace=True)
 df_public.replace(to_replace='4e+05', value='400000', inplace=True)
 df_public.replace(to_replace='6e+05', value='600000', inplace=True)
 df_public.replace(to_replace='9e+05', value='900000', inplace=True)
+df_public.replace(to_replace="Yes", value='1', inplace=True)
+df_public.replace(to_replace="No", value='0', inplace=True)
+
+onehotencoder = LabelEncoder()
+df_public['JobSat'] = onehotencoder.fit_transform(df_public['JobSat'])
 
 # change all to float
 df_public = df_public.astype(float)
 
+pd.set_option('display.max_columns', None)
 print(df_public.corr())
-
-plt.plot(df_public['YearsCodePro'],
-         df_public['ConvertedComp'], 'ro', markersize=0.3)
-plt.xlabel('YearsCodePro')
-plt.ylabel('ConvertedComp')
-plt.title('All')
-plt.show()
