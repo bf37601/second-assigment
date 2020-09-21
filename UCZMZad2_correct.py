cc99685b1b9ef2c9402c326dafd5f44cb3d1d8c9
bf37601 <bf37601@st.amu.edu.pl>
2020-09-21 21:20:30 +0200

    #Task 4 correct

--- UCZMZad2_correct.py
+++ UCZMZad2_correct.py
@@ -2,7 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from sklearn import linear_model
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
 
 df_public = pd.read_csv('survey_results_public.csv',
                         usecols=['Respondent',
@@ -37,13 +39,13 @@ df_public['JobSat'] = onehotencoder.fit_transform(df_public['JobSat'])
 # change all to float
 df_public = df_public.astype(float)
 
-
-# y = ConvertedComp
+# y = Age
 # x1 = YearsCodePro
-# x2 = Age
+# x2 = ConvertedComp
 
 # +-3 standard deviation elimination
-df_public_sd = df_public[np.abs(df_public - df_public.mean()) <= 3*df_public.std()]
+df_public_sd = df_public[np.abs(df_public - df_public.mean()) <=
+                         3*df_public.std()]
 df_public_sd.isna().sum()
 df_public_sd = df_public_sd.dropna()
 
@@ -51,14 +53,14 @@ df_public_sd = df_public_sd.dropna()
 Q1 = df_public.quantile(0.25)
 Q3 = df_public.quantile(0.75)
 IQR = Q3 - Q1
-df_public_q = df_public[~((df_public < (Q1 - 1.5 * IQR)) | (df_public > (Q3 + 1.5 * IQR))).any(axis=1)]
+df_public_q = df_public[~((df_public < (Q1 - 1.5 * IQR)) |
+                          (df_public > (Q3 + 1.5 * IQR))).any(axis=1)]
 
 pd.set_option('display.max_columns', None)
 
 print(df_public.corr())
 print(df_public_sd.corr())
 print(df_public_q.corr())
-
 
 df_public.plot()
 plt.show()
@@ -79,3 +81,31 @@ sns.boxplot(y='Age', data=df_public_sd)
 plt.show()
 sns.boxplot(y='Age', data=df_public_q)
 plt.show()
+
+reg = linear_model.LinearRegression()
+
+df_len = len(df_public_q)
+y_true = df_public_q['Age']
+
+# x1
+# x_test = df_public_q[['YearsCodePro']]
+# reg.fit(x_test, y_true)
+
+# x2
+# x_test = df_public_q[['ConvertedComp']]
+# reg.fit(x_test, y_true)
+
+# y_test = pd.DataFrame(np.random.rand(df_len)*100)  # for x1 or x2
+
+# x1 & x2
+# x_test = df_public_q[['YearsCodePro', 'ConvertedComp']]
+# reg.fit(x_test, y_true)
+# y_test = pd.DataFrame(np.random.rand(df_len,2)*100)
+
+# x1 & x2 & 'BetterLife' & 'JobSat'
+x_test = df_public_q[['YearsCodePro', 'ConvertedComp', 'BetterLife', 'JobSat']]
+reg.fit(x_test, y_true)
+y_test = pd.DataFrame(np.random.rand(df_len, 4)*100)
+
+y_pred = reg.predict(x_test)
+print(mean_squared_error(y_true, y_pred))
