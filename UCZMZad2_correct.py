9cd7d33ad605c060326acc1778dd59f77ab26e72
bf37601 <bf37601@st.amu.edu.pl>
2020-09-21 21:30:46 +0200

    Task #5 correct

--- UCZMZad2_correct.py
+++ UCZMZad2_correct.py
@@ -8,8 +8,6 @@ from sklearn.metrics import mean_squared_error
 
 df_public = pd.read_csv('survey_results_public.csv',
                         usecols=['Respondent',
-                                 'Age1stCode',
-                                 'YearsCode',
                                  'YearsCodePro',
                                  'ConvertedComp',
                                  'Age',
@@ -39,16 +37,6 @@ df_public['JobSat'] = onehotencoder.fit_transform(df_public['JobSat'])
 # change all to float
 df_public = df_public.astype(float)
 
-# y = Age
-# x1 = YearsCodePro
-# x2 = ConvertedComp
-
-# +-3 standard deviation elimination
-df_public_sd = df_public[np.abs(df_public - df_public.mean()) <=
-                         3*df_public.std()]
-df_public_sd.isna().sum()
-df_public_sd = df_public_sd.dropna()
-
 # quantile elimination
 Q1 = df_public.quantile(0.25)
 Q3 = df_public.quantile(0.75)
@@ -59,25 +47,16 @@ df_public_q = df_public[~((df_public < (Q1 - 1.5 * IQR)) |
 pd.set_option('display.max_columns', None)
 
 print(df_public.corr())
-print(df_public_sd.corr())
 print(df_public_q.corr())
 
 df_public.plot()
 plt.show()
-df_public_sd.plot()
-plt.show()
 df_public_q.plot()
 plt.show()
 
-sns.boxplot(y='ConvertedComp', data=df_public_sd)
-plt.show()
 sns.boxplot(y='ConvertedComp', data=df_public_q)
 plt.show()
-sns.boxplot(y='YearsCodePro', data=df_public_sd)
-plt.show()
 sns.boxplot(y='YearsCodePro', data=df_public_q)
-plt.show()
-sns.boxplot(y='Age', data=df_public_sd)
 plt.show()
 sns.boxplot(y='Age', data=df_public_q)
 plt.show()
@@ -86,21 +65,6 @@ reg = linear_model.LinearRegression()
 
 df_len = len(df_public_q)
 y_true = df_public_q['Age']
-
-# x1
-# x_test = df_public_q[['YearsCodePro']]
-# reg.fit(x_test, y_true)
-
-# x2
-# x_test = df_public_q[['ConvertedComp']]
-# reg.fit(x_test, y_true)
-
-# y_test = pd.DataFrame(np.random.rand(df_len)*100)  # for x1 or x2
-
-# x1 & x2
-# x_test = df_public_q[['YearsCodePro', 'ConvertedComp']]
-# reg.fit(x_test, y_true)
-# y_test = pd.DataFrame(np.random.rand(df_len,2)*100)
 
 # x1 & x2 & 'BetterLife' & 'JobSat'
 x_test = df_public_q[['YearsCodePro', 'ConvertedComp', 'BetterLife', 'JobSat']]
